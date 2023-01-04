1a30d52c2a0280952ad69207224782c1d008d6a2
anikeydev <dev.anikey.va@mail.ru>
2023-01-04 20:23:57 +0300

    created handler click on box

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const $start = document.querySelector('#start')
 const $game = document.querySelector('#game')
 
 $start.addEventListener('click', start)
+$game.addEventListener('click', handlerBoxClick)
 
 function start() {
   $start.classList.add('hide')
@@ -10,15 +11,22 @@ function start() {
   renderBox()
 }
 
-function renderBox() {
+function handlerBoxClick(event) {
+  if (event.target.dataset.box) {
+    renderBox('60px')
+  }
+}
+
+function renderBox(s = '50px') {
   const box = document.createElement('div')
 
-  box.style.height = box.style.width = '50px'
+  box.style.height = box.style.width = s
   box.style.position = 'absolute'
   box.style.backgroundColor = '#000'
   box.style.top = '50px'
   box.style.left = '70px'
   box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
 
   $game.insertAdjacentElement('afterbegin', box)
 }
