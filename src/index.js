df34e785a12f00c08c98539391a729d5ede0d721
anikeydev <dev.anikey.va@mail.ru>
2023-01-04 20:19:33 +0300

    created function renderBox

--- src/index.js
+++ src/index.js
@@ -6,4 +6,19 @@ $start.addEventListener('click', start)
 function start() {
   $start.classList.add('hide')
   $game.style.backgroundColor = '#fff'
+
+  renderBox()
+}
+
+function renderBox() {
+  const box = document.createElement('div')
+
+  box.style.height = box.style.width = '50px'
+  box.style.position = 'absolute'
+  box.style.backgroundColor = '#000'
+  box.style.top = '50px'
+  box.style.left = '70px'
+  box.style.cursor = 'pointer'
+
+  $game.insertAdjacentElement('afterbegin', box)
 }
